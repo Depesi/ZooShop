3b94d550f3f38a82e9f3838bd5823304cf7e13e2
Popchiyk <roma240103@gmail.com>
2022-10-14 13:30:10 +0300

    init project

---
+++ backend/zooShop/src/main/java/ua/bg/zooshop/ZooShopApplication.java
@@ -0,0 +1,13 @@
+package ua.bg.zooshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZooShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZooShopApplication.class, args);
+	}
+
+}
---
+++ backend/zooShop/src/test/java/ua/bg/zooshop/ZooShopApplicationTests.java
@@ -0,0 +1,13 @@
+package ua.bg.zooshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZooShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
