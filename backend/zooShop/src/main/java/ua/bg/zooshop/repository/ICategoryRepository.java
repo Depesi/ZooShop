f8d95e3ea49ff094b2a5aada806b1ea724e80249
Popchiyk <roma240103@gmail.com>
2022-10-21 16:31:34 +0300

    mistake commit

--- backend/zooShop/src/main/java/ua/bg/zooshop/repository/ICategoryRepository.java
+++
@@ -1,9 +0,0 @@
-package ua.bg.zooshop.repository;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.stereotype.Repository;
-import ua.bg.zooshop.entity.Category;
-
-@Repository
-public interface ICategoryRepository extends MongoRepository<Category,Integer> {
-}
--- backend/zooShop/src/main/java/ua/bg/zooshop/repository/IOrderRepository.java
+++
@@ -1,9 +0,0 @@
-package ua.bg.zooshop.repository;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.stereotype.Repository;
-import ua.bg.zooshop.entity.Order;
-
-@Repository
-public interface IOrderRepository extends MongoRepository<Order, Integer> {
-}
