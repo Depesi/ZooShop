bc39ee2a6dff47af80e4aaf3ba5089304e254071
Popchiyk <roma240103@gmail.com>
2022-10-28 15:58:51 +0300

    EncoderPassword + edit update

--- backend/zooShop/src/main/java/ua/bg/zooshop/config/EncoderPassword.java
+++ backend/zooShop/src/main/java/ua/bg/zooshop/config/EncoderPassword.java
@@ -1,7 +1,5 @@
 package ua.bg.zooshop.config;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 
